d9a5c742c69b89b63cb875910573d404fcfb9a3b
maotaltsutaja62 <65294984+maotaltsutaja62@users.noreply.github.com>
2020-06-02 18:19:48 +0300

    Update for ruudus.py

--- Tund3/for ruudus.py
+++ Tund3/for ruudus.py
@@ -1,8 +1,10 @@
 import math
 number = 20
 for x in range (number):
+    if x == 0:
+        continue
     
-    if x%2 ==0:#% võtab jäägi,kui on 0,siis on paarisarv
+    elif x%2 ==0:#% võtab jäägi,kui on 0,siis on paarisarv
         print(math.sqrt(x))
     else:
         print(x**2)
